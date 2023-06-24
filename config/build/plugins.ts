a2d84c9f006c793b1b422b505e7d95c15436a6d2
Dimitry Nasrtdinov <hollowiccup@gmail.com>
2023-06-24 08:09:19 +0300

    edit main.yml

--- config/build/plugins.ts
+++ config/build/plugins.ts
@@ -3,21 +3,29 @@ import webpack from 'webpack'
 import MiniCssExtractPlugin from 'mini-css-extract-plugin'
 import { type BuildOptions } from './types/config'
 import { BundleAnalyzerPlugin } from 'webpack-bundle-analyzer'
-export const buildPlugins = ({ paths, isDev }: BuildOptions): webpack.WebpackPluginInstance[] => [
-  new webpack.ProgressPlugin(),
-  new HtmlWebpackPlugin({
-    template: paths.html,
-  }),
-  new MiniCssExtractPlugin({
-    filename: 'css/[name].[contenthash:8].css',
-    chunkFilename: 'css/[name].[contenthash:8].css',
-  }),
-  new webpack.DefinePlugin({
-    // eslint-disable-next-line @typescript-eslint/naming-convention
-    __IS_DEV__: JSON.stringify(isDev),
-  }),
-  new webpack.HotModuleReplacementPlugin(),
-  new BundleAnalyzerPlugin({
-    openAnalyzer: false,
-  }),
-]
+
+export function buildPlugins({ paths, isDev }: BuildOptions): webpack.WebpackPluginInstance[] {
+  const plugins = [
+    new webpack.ProgressPlugin(),
+    new HtmlWebpackPlugin({
+      template: paths.html,
+    }),
+    new MiniCssExtractPlugin({
+      filename: 'css/[name].[contenthash:8].css',
+      chunkFilename: 'css/[name].[contenthash:8].css',
+    }),
+    new webpack.DefinePlugin({
+      // eslint-disable-next-line @typescript-eslint/naming-convention
+      __IS_DEV__: JSON.stringify(isDev),
+    }),
+  ]
+  if (isDev) {
+    plugins.push(new webpack.HotModuleReplacementPlugin())
+    plugins.push(
+      new BundleAnalyzerPlugin({
+        openAnalyzer: false,
+      }),
+    )
+  }
+  return plugins
+}
